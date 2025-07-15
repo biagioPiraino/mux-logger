3ed06280db42254307da63cae4d23b3a189677dd
biagioPiraino <biagiopirainowork@gmail.com>
2025-07-15 06:10:53 +0100

    logger - getting requestId from context

--- muxlogger.go
+++ muxlogger.go
@@ -9,6 +9,10 @@ import (
 	"time"
 )
 
+type contextKey string
+
+const requestIdKey = contextKey("requestId")
+
 type WrappedResponseWriter struct {
 	http.ResponseWriter
 	statusCode int
@@ -39,8 +43,8 @@ func Logger(next http.Handler) http.Handler {
 		log.SetFlags(0)
 		log.SetOutput(file)
 
-		// get request id from header passed from the client or injected by middleware
-		requestId := r.Header.Get("X-Request-ID")
+		// get request id from the request context
+		requestId := r.Context().Value(requestIdKey).(string)
 
 		defer closeLogFile(file)
 
